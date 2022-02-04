1e92db1e1795c576dface11522b0a5e107ef54a6
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-02-03 23:03:33 -0300

    test: first

---
+++ tests/routes/index.spec.ts
@@ -0,0 +1,20 @@
+import supertest from "supertest";
+
+import app from "../../src/app";
+import { Details } from "../../src/interfaces";
+
+describe("Route /details", () => {
+  test("GET - /details", async () => {
+    const expectedBody: Details = {
+      version: process.env.npm_package_version,
+      author: process.env.npm_package_author_name,
+      email: process.env.npm_package_author_email,
+      repository_url: process.env.npm_package_repository_url,
+    };
+
+    const response = await supertest(app).get("/details");
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(expectedBody);
+  });
+});
